fffc5a860d13b6cd50f7a60a704ad08e023f98e2
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-04-27 12:13:41 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -7,6 +7,24 @@
     <title>Document</title>
 </head>
 <body>
+<h1>Vypsaná data : </h1>
+<?php
+      $username = filter_input(INPUT_POST,'username');
+      $password = filter_input(INPUT_POST,'password');
+      $gender = filter_input(INPUT_POST,'gender');
+      $email = filter_input(INPUT_POST,'email');
+      $City = filter_input(INPUT_POST,'City');
+      $jmeno = filter_input(INPUT_POST,'jmeno');
+      echo "Bylo vyplněno jméno $username <br>";
+      echo "Bylo vyplněno vaše heslo $password";
+
+     if (($username =='Jaroslav Melichar') && ($password == '1892')){
+         echo  "úspěšně jste se přihlásil";}
+    else {
+    echo "Nepodařilo se přihlásit";}
+
+      
+      ?>
     
 </body>
 </html>
